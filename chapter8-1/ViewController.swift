3b3e55505380cef811412f2cf5cb1a419ad0e5f9
SMB開発部 <sorimachi-aws@hotmail.com>
2014-11-19 16:28:26 +0900

    Chapter8-1
    Tableを使用してのデータ表示

--- chapter8-1/ViewController.swift
+++ chapter8-1/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +19,21 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    //行数
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    //セルの中身
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = UITableViewCell(style: .Default, reuseIdentifier: "myCell")
+        cell.textLabel.text = "\(indexPath.row)行目"
+        return cell
+    }
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        println("\(indexPath.row)行目を選択")
+    }
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
 }
 
